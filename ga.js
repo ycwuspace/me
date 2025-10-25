e2a2075cbaf52ddfd7a01c7f1ef7e68d53caaf7f
ycwu <s0960131@gmail.com>
2025-10-25 09:16:45 +0800

    v1

---
+++ ga.js
@@ -0,0 +1,14 @@
+// 請將 G-XXXXXXXXXX 換成你的實際測量 ID
+(function() {
+  var gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZS2N2C2NBP';
+  document.head.appendChild(gaScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-ZS2N2C2NBP');
+})();
